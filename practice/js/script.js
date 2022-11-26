527166c9e4ca5ac61dcd847e4cbcbb50b87442aa
BukinDaniel <86843809+BukinDaniel@users.noreply.github.com>
2022-11-26 17:40:09 +0300

    prepare popup and do slider

--- practice/js/script.js
+++ practice/js/script.js
@@ -7,4 +7,42 @@ $(function() {
         preloader: false,
         fixedContentPos: false
     });
+
+    $('.gallary__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-right.svg" alt=""></button>'
+    });
+
+    $('.galary__item-inner').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+
+    });
+
+    $('.galary__item-inner').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 400, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
 });
