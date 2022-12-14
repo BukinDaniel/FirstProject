74169645c9a81e7e96760be79f64d90387e11769
BukinDaniel <86843809+BukinDaniel@users.noreply.github.com>
2022-12-14 17:50:38 +0300

    tillTheEND!

---
+++ NewProjectAuto/project/js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    $('.auto__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+
+
+
+
+
+});
