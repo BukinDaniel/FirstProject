8a8231f956db239472e6b503667ca39c0242c3b8
BukinDaniel <86843809+BukinDaniel@users.noreply.github.com>
2022-12-16 20:36:40 +0300

    EndAutoCar

--- NewProjectAuto/project/js/main.js
+++ NewProjectAuto/project/js/main.js
@@ -6,11 +6,31 @@ $(function() {
         slidesToShow: 3,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            },
 
+
+        ]
     });
 
+});
 
-
-
-
+$(function() {
+    $("a.scrollto").click(function() {
+        let elementClick = $(this).attr("href")
+        let destination = $(elementClick).offset().top;
+        $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 2500);
+        return false;
+    });
 });
